d084e87c54f6f6e2d98939696c909d694003a3dc
mahadirerfin <muhammaderfin251@gmail.com>
2022-05-15 10:38:58 +0700

    mengerjakan scene1 ada di asset Scene1

---
+++ Assets/New-Asset/Script/EnemyPatrol_1.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol_1 : MonoBehaviour
+{
+    [HideInInspector]
+    public bool mustPatrol;
+    private bool mustFlip;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private Transform groundCheck;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    [SerializeField]
+    private Collider2D body;
+
+    [SerializeField]
+    private float walkSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mustPatrol = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mustPatrol)
+        {
+            Patrol();            
+        }        
+    }
+
+    void FixedUpdate()
+    {
+        if(mustPatrol)
+        {
+            mustFlip = !Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        }
+    }
+
+    void Patrol()
+    {
+        if(mustFlip || body.IsTouchingLayers(groundLayer))
+        {
+            Flip();
+        }
+        rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        mustPatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        mustPatrol = true;
+    }
+}
