788f5e1ea018c89442578979b3d26927c91a388e
dickyarya <dickyarya170101@gmail.com>
2022-06-07 23:11:05 +0700

    menambah start menu, pause menu dan kondisi mati dodit

---
+++ Assets/Script/SceneSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private int SceneIndex;
+
+    public void forwardScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void backScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Debug.Log("back");
+
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    public void moveToScene()
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
--- Assets/Script/hoverMenu.cs
+++ Assets/Script/hoverMenu.cs
@@ -5,19 +5,52 @@ using UnityEngine;
 public class hoverMenu : MonoBehaviour
 {    // Update is called once per frame
     [SerializeField] private GameObject panelMenu;
-    
+
+    [SerializeField] private bool isPauseMenu;
+
     void Update()
+    {
+        if(isPauseMenu == true)
+        {
+            PauseMenu();
+        }else {
+            if(panelMenu.active == true && Input.GetKeyDown(KeyCode.Escape))
+            {
+                panelMenu.SetActive(false);
+            }
+
+        }
+
+    }
+
+    public void PauseMenu()
     {
         if (Input.GetKeyDown(KeyCode.Escape) ) 
         {
             if (panelMenu.active == true)
             {
                 panelMenu.SetActive(false);
+                Time.timeScale = 1;
             }else
             {
                 panelMenu.SetActive(true);
+                Time.timeScale = 0;
             }
         } 
+
+    }
+
+    public void openMenu()
+    {
+         panelMenu.SetActive(true);
+
+    }
+
+    public void resumeGame()
+    {
+         panelMenu.SetActive(false);
+         Time.timeScale = 1;
+
     }
 
 }
